94c518b03a3063fe3203b89bc0c60626110e9b04
htien <mac@Macs-MacBook-Pro.local>
2021-09-02 02:30:39 +0700

    add responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,50 @@
-const menuIcon = document.getElementsByClassName("header__menu-icon-container")[0];
+const menuIconSlide = document.getElementsByClassName("menu-pc-slide")[0];
+const menuIconMobile = document.getElementsByClassName("menu-mobile-nav")[0];
 const menuSlide = document.getElementsByClassName("menu-slide")[0];
 const menuSlideCloseBtn = document.getElementsByClassName("menu-slide__close")[0];
+const mobileNavContainer = document.getElementsByClassName("nav-container")[0];
+const mobileNavCloseBtn = document.getElementsByClassName("mobile-nav__close")[0];
+const nav = document.getElementsByClassName('nav')[0];
+ 
 
-menuIcon.onclick = function () {
-  menuSlide.classList.remove("animation--slide-out");
+menuIconSlide.onclick = function () {
+  menuSlide.classList.remove("animation--slide-out"); 
   menuSlide.classList.add("animation--slide-in");
-  menuSlide.style.visibility = "visible";
+  menuSlide.classList.add('visible');
+  menuSlide.classList.remove('not-visible');
 };
+
+menuIconMobile.onclick = function () {
+  mobileNavContainer.classList.remove("animation--slide-out");
+  mobileNavContainer.classList.add("animation--slide-in");
+  mobileNavContainer.classList.add('visible');
+  mobileNavContainer.classList.remove('not-visible');
+
+};
+
 menuSlideCloseBtn.onclick = function () {
   menuSlide.classList.remove("animation--slide-in");
   menuSlide.classList.add("animation--slide-out");
   setTimeout(function () {
-    menuSlide.style.visibility = "hidden";
+  menuSlide.classList.add('not-visible');  
+  menuSlide.classList.remove('visible');
+
+    
   }, 480);
 };
+
+
+mobileNavCloseBtn.onclick = function () {
+  mobileNavContainer.classList.remove("animation--slide-in");
+  mobileNavContainer.classList.add("animation--slide-out");
+  setTimeout(function () {
+  mobileNavContainer.classList.remove('visible');
+  mobileNavContainer.classList.add('not-visible');  
+  }, 480);
+};
+
+window.addEventListener('resize', function(event) {
+  if(mobileNavContainer.classList.contains('not-visible')) {
+    mobileNavContainer.classList.remove('not-visible');
+  }
+}, true);
